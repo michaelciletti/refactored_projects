a59e1fb22833d83e7a8e68e86a29027b2ce58f9f
michaelciletti <michaelciletti@gmail.com>
2017-02-09 16:54:57 -0500

    passing test 3

--- minedmindsre.rb
+++ minedmindsre.rb
@@ -2,7 +2,8 @@ def refactored_mined_minds_kata(i)
 
 	if i % 15 == 0             
    		return "mined minds"        
-        
+     elsif i % 3 == 0            
+   		return "mined"  
    	else            
    		return i        
    	end
